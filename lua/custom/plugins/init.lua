d80c16a96a2b158cd5c94a7d2f8870ca50686f4c
Andreas Eben <andreas.eben1995@gmail.com>
2025-01-21 11:30:05 +0100

    Remove kitty-scrollback plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -42,18 +42,6 @@ return {
   { 'sindrets/diffview.nvim' },
   { 'mfussenegger/nvim-jdtls' },
   {
-    'mikesmithgh/kitty-scrollback.nvim',
-    enabled = true,
-    lazy = true,
-    cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth' },
-    event = { 'User KittyScrollbackLaunch' },
-    -- version = '*', -- latest stable version, may have breaking changes if major version changed
-    version = '^5.0.0', -- pin major version, include fixes and features that do not have breaking changes
-    config = function()
-      require('kitty-scrollback').setup()
-    end,
-  },
-  {
     'JoosepAlviste/nvim-ts-context-commentstring',
     config = function ()
       require('ts_context_commentstring').setup {
